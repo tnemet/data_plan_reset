7bfa8beae598f1fe81611fb20d3c110304b4151f
tnemet <itnemet@protonmail.com>
2023-12-08 15:13:12 +0100

    show daily data usage

--- data_counter.sh
+++ data_counter.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-limit=4
+#set limit in GB
+limit=5
+#check usage, RX only. 4th field of vnstat output
+#vnstat saveInterval is in minutes so it should be worked around to work in seconds 
+#(deamon.c void adjustsaveinterval(DSTATE *s))
+used=$(vnstat --oneline | cut -d ';' -f 4)
+used_int=$(awk -v used="$used" 'BEGIN { printf "%.0f", used }')
 
-used=$(vnstat --oneline | cut -d ';' -f 11)
-used_int=$(expr "$used" : '\([0-9]*\)')
-
+echo "rounded: $used_int" 
 echo "limit: $limit"
-echo "iskoristeno: $used"
+echo "used: $used"
 
 if [ $used_int -ge $limit ]
 then   
