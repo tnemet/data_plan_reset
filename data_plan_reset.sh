e783bfcf08e0002cb4f07c3e0cd0312c20381f6d
tnemet <itnemet@protonmail.com>
2023-12-13 15:02:01 +0100

    v1.0

--- data_plan_reset.sh
+++ data_plan_reset.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 
+# With concern about execution time for this script, considering options (what)TO-DO:
+#   - make 2 scripts, one to check limit and other one to send SMS code if neccessary
+#   - research for some kind of watchdog to monitor vnstat database for changes
+# 5 hours later research done.
+# Zabbix is the answer, so will try to configure zabbix agent for ths purpose
+
 #set limit in GB
 limit=5
 
@@ -10,13 +17,13 @@ DESTNUM="095200100"
 # Message
 SMS="NEOGRANICENO"
 
-#check usage, RX only. 4th field of vnstat output
-#vnstat saveInterval is in minutes so it should be worked around to work in seconds 
-#(deamon.c void adjustsaveinterval(DSTATE *s))
+# check usage, RX only. 4th field of vnstat output
+# vnstat saveInterval is in minutes so it should be worked around to work in seconds 
+# (deamon.c void adjustsaveinterval(DSTATE *s))
 used=$(vnstat --oneline | cut -d ';' -f 4)
 used_int=$(awk -v used="$used" 'BEGIN { printf "%.0f", used }')
 
-#echo values, for testing only
+# echo values, for testing only
 echo "limit: $limit"
 echo "used: $used"
 echo "rounded: $used_int" 
